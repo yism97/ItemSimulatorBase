e709f3a7c96a74d2bddf1e73aaf85459c216db40
NamcheolHer <yihs97@naver.com>
2024-09-11 15:58:51 +0900

    add : 로그인,회원가입 기능 추가

--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import joi from 'joi';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -13,9 +16,96 @@ const prisma = new PrismaClient({
 // Request의 Authorization 헤더에서 JWT를 가져와서 인증 된 사용자인지 확인하는 Middleware를 구현합니다
 
 // 6-1. [도전] 회원가입
-router.post('/account/join', (req, res) => {});
+router.post('/account/join', async (req, res) => {
+  try {
+    const joinSchema = joi.object({
+      accountId: joi.string().alphanum().lowercase().required(),
+      password: joi.string().min(6).required(),
+      confirmPassword: joi.valid(joi.ref('password')).required(),
+      userName: joi.string().required(),
+    });
+
+    const validateResult = joinSchema.validate(req.body);
+    console.log(validateResult);
+    if (validateResult.error) {
+      res.status(400).json({ error: '입력된 값이 잘못됐어요.' });
+      return;
+    }
+    const inputValue = validateResult.value;
+
+    const accountId = req.body.accountId; // 영어 소문자랑 숫자로만 구성 필요
+    const password = req.body.password; // 최소 여섯자 이상이며 비밀번호 확인과 일치해야 한다.
+    const userName = req.body.userName;
+
+    // 비밀번호는 평문으로 쓰지말고 해싱해서 저장필요
+    // 단방향 암호화 !
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const existAccount = await prisma.account.findUnique({
+      where: {
+        accountId: accountId,
+      },
+    });
+    if (existAccount) {
+      res.status(400).json({ error: '중복된 아이디에요!' });
+      return;
+    }
+
+    const joinAccount = await prisma.account.create({
+      data: {
+        accountId: accountId,
+        password: hashedPassword,
+        userName: userName,
+      },
+    });
+
+    res.status(200).json({
+      message: '회원가입이 완료 되었습니다.',
+      account_info: { accountId: joinAccount.accountId, userName: joinAccount.userName },
+    }); // 회원가입 시, 비밀번호를 제외 한 사용자의 정보를 반환
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const users = {};
 
 // 6-2. [도전] 로그인
-router.post('/account/login', (req, res) => {});
+// 아이디, 비밀번호, 비밀번호 확인, 이름 넘기기
+router.post('/account/login', async (req, res) => {
+  const loginSchema = joi.object({
+    accountId: joi.string().alphanum().lowercase().required(),
+    password: joi.string().min(6).required(),
+  });
+
+  const validateResult = loginSchema.validate(req.body);
+  if (validateResult.error) {
+    res.status(400)({ error: '잘못된 요청입니다.' });
+    return;
+  }
+
+  const inputValue = validateResult.value;
+  const accountId = inputValue.accountId;
+  const password = inputValue.password;
+
+  const account = await prisma.account.findUnique({ where: { accountId: accountId } });
+  if (account == null) {
+    res.status(400).json({ error: '계정이 존재하지 않습니다.' });
+    return;
+  }
+
+  const passwordValidate = await bcrypt.compare(password, account.password); // 입력한 password와 등록된 password 일치 여부 확인
+  if (!passwordValidate) {
+    res.status(400).json({ error: '비밀번호가 일치하지 않습니다.' });
+    return;
+  }
+
+  const accessToken = jwt.sign(
+    { accountId: accountId, userName: account.userName },
+    'secretOrPrivateKey',
+    { expiresIn: '1h' },
+  );
+
+  res.status(200).json({ account_info: { accessToken: accessToken } });
+});
 
 export default router;
